32c366284a3164482338e9ec2c5c91f09d03aa56
Q100SZ71 <103309173+Q100SZ71@users.noreply.github.com>
2022-04-10 09:47:52 +0200

    zmiana wiadomosci

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 9000;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Testowanie jest super SUPER!');
+  res.end('Testowanie jest super EXTRA!');
 }
 
 const server = http.createServer(requestListener);
