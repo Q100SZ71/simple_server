bb3be8eb3df85c923014cd2ba066c0c5d4cf76c2
Q100SZ71 <q100sz71@op.pl>
2022-04-10 09:36:07 +0200

    dodanie servera i jsona

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const port = process.env.PORT || 9000;
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Testowanie jest super SUPER!');
+}
+
+const server = http.createServer(requestListener);
+server.listen(port);
